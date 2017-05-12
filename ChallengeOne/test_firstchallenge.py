b70ccb71e26874c4e0d671a57a4c94c12b0ded44
Christopher Geiger <christophergeiger3@gmail.com>
2017-05-11 21:06:00 -0400

    Add ToDo, solve import error

--- ChallengeOne/test_firstchallenge.py
+++ ChallengeOne/test_firstchallenge.py
@@ -1,2 +1,6 @@
 from FirstChallenge import *
 
+# Note! If this file does not run in pycharm, go to the folder containing this file, right click,
+# select "Mark Directory as" and then "Sources Root" . Things should start working from there.
+
+# ToDO: Implement test file; work on other challenges
